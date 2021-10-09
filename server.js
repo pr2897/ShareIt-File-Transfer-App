0eb094c8cf11494458cd7dc86fd2e5dba5cfacc2
PIYUSH RAJ <piyushrajkhg@gmail.com>
2021-10-09 13:37:16 +0530

    added root route for ease

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(express.json());
@@ -9,9 +9,13 @@ app.use(express.json());
 connectDB();
 
 //Routes
-app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'));
-app.use('/files/download', require('./routes/download'));
+
+app
+  .route("/")
+  .get((req, res) => res.status(200).json({ message: "API Online" }));
+app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
 
 const port = app.listen(process.env.PORT, () =>
   console.log(`Listening on port: ${process.env.PORT}`)
